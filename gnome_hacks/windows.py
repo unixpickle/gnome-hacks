5770fee228af976bbe65e8e881a8e385da139c9d
Alex Nichol <unixpickle@gmail.com>
2021-10-03 09:12:07 -0400

    get display geometry; move windows

--- gnome_hacks/windows.py
+++ gnome_hacks/windows.py
@@ -56,3 +56,43 @@ def get_window_frame(e: Evaluator, id: int) -> Optional[Rect]:
     """
     result = e(code, window_id=id)
     return None if result is None else Rect(**result)
+
+
+def get_window_monitor_frame(e: Evaluator, id: int) -> Optional[Rect]:
+    code = """
+    const actors = global.get_window_actors();
+    let result = null;
+    for (let i = 0; i < actors.length; i++) {
+        const window = actors[i].get_meta_window();
+        if (window.get_id() == window_id) {
+            const display = window.get_display();
+            const monitor = window.get_monitor();
+            const f = display.get_monitor_geometry(monitor);
+            result = {
+                x: f.x,
+                y: f.y,
+                width: f.width,
+                height: f.height,
+            };
+        }
+    }
+    result;
+    """
+    result = e(code, window_id=id)
+    return None if result is None else Rect(**result)
+
+
+def move_window(e: Evaluator, id: int, x: int, y: int) -> bool:
+    code = """
+    const actors = global.get_window_actors();
+    let result = false;
+    for (let i = 0; i < actors.length; i++) {
+        const window = actors[i].get_meta_window();
+        if (window.get_id() == window_id) {
+            window.move_frame(0, x, y);
+            result = true;
+        }
+    }
+    result;
+    """
+    return e(code, window_id=id, x=x, y=y)
