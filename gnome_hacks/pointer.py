8b0ddef698b74f85408e5ceaff1b2905b9817f94
Alex Nichol <unixpickle@gmail.com>
2021-10-18 16:02:57 -0400

    faster pointer events

--- gnome_hacks/pointer.py
+++ gnome_hacks/pointer.py
@@ -9,7 +9,7 @@ from .evaluator import Evaluator
 class PointerMove:
     x: Union[int, float]
     y: Union[int, float]
-    delay_ms: int = 100
+    delay_ms: int = 0
 
     def encode_pointer_event(self) -> Dict[str, Any]:
         return dict(move=dict(x=self.x, y=self.y, delay_ms=self.delay_ms))
@@ -19,9 +19,14 @@ class PointerMove:
 class PointerButton:
     pressed: bool
     button: int = 1
+    delay_ms: int = 100
 
     def encode_pointer_event(self) -> Dict[str, Any]:
-        return dict(button=dict(pressed=self.pressed, button=self.button))
+        return dict(
+            button=dict(
+                pressed=self.pressed, button=self.button, delay_ms=self.delay_ms
+            )
+        )
 
 
 def simulate_pointer_events(
@@ -45,6 +50,11 @@ def simulate_pointer_events(
     for (let i = 0; i < events.length; i++) {
         const e = events[i];
         if (e.button) {
+            // For some reason, some click events don't work if they are
+            // directly after a mouse movement, so clicks can have a delay
+            // before them.
+            await waitPromise(e.button.delay_ms);
+
             dev.notify_button(
                 0,
                 e.button.button,
